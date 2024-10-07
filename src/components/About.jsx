da21bab532950b2e10aa8753113d5f734416c69e
Maniha-Affaf <affaf.maniha@gmail.com>
2024-10-07 23:59:13 +0500

    checked responsiveness

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,19 +4,19 @@ import { ABOUT_TEXT } from "../constants";
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-12 text-center text-3xl sm:my-20 sm:text-4xl">
+      <h2 className="my-12 text-center text-3xl xs:my-20 xs:text-4xl">
         About
         <span className="text-neutral-500"> Me</span>
       </h2>
       <div className="flex flex-wrap lg:flex-nowrap">
         {/* Image Section */}
         <div className="w-full lg:w-1/2 lg:p-8 flex justify-center mb-6 lg:mb-0">
-          <img className="rounded-2xl max-w-xs sm:max-w-sm lg:max-w-full" src={aboutImg} alt="About Pic" />
+          <img className="rounded-2xl max-w-xs xs:max-w-xs lg:max-w-full" src={aboutImg} alt="About Pic" />
         </div>
 
         {/* Text Section */}
-        <div className="w-full lg:w-1/2 flex justify-center lg:justify-start">
-          <p className="my-2 max-w-xl py-6 px-4 text-center lg:text-left font-light">
+        <div className="w-full lg:w-1/2 flex xs:justify-start lg:justify-start">
+          <p className="my-2 max-w-xl py-6 px-4 xs:text-left sm:text-left lg:text-left font-light">
             {ABOUT_TEXT}
           </p>
         </div>
